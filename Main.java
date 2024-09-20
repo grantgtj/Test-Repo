06e05397b1e4c9c712980f9a87d30504c7747239
Grant Johnson <grantgtj@gmail.com>
2024-09-20 14:57:16 +0200

    add third test (in test branch)

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Test Change!");
         System.out.println("Test Change2!");
+        System.out.println("Test Change3 (in test branch)!");
     }
 }
